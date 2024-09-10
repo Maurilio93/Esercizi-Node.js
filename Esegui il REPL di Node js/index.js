ab34b54d78c3dc055fa68fef775a91b2670baa53
Maurilio Valenti <166375402+Maurilio93@users.noreply.github.com>
2024-09-10 11:02:29 +0200

    Add files via upload

---
+++ Esegui il REPL di Node js/index.js
@@ -0,0 +1,92 @@
+// PS C:\Users\gabri\OneDrive\Desktop\ESERCIZI NODE.JS\Esegui il REPL di Node js> node
+// Welcome to Node.js v18.15.0.
+// Type ".help" for more information.
+// > .help
+// .break    Sometimes you get stuck, this gets you out
+// .clear    Alias for .break
+// .editor   Enter editor mode
+// .exit     Exit the REPL
+// .help     Print this help message
+// .load     Load JS from a file into the REPL session
+// .save     Save all evaluated commands in this REPL session to a file
+
+// Press Ctrl+C to abort current expression, Ctrl+D to exit the REPL
+// > const crypto = require ("crypto")
+// undefined
+// > console.log(Object.keys(crypto))
+// [
+//   'checkPrime',
+//   'checkPrimeSync',
+//   'createCipheriv',
+//   'createDecipheriv',
+//   'createDiffieHellman',
+//   'createDiffieHellmanGroup',
+//   'createECDH',
+//   'createHash',
+//   'createHmac',
+//   'createPrivateKey',
+//   'createPublicKey',
+//   'createSecretKey',
+//   'createSign',
+//   'createVerify',
+//   'diffieHellman',
+//   'generatePrime',
+//   'generatePrimeSync',
+//   'getCiphers',
+//   'getCipherInfo',
+//   'getCurves',
+//   'getDiffieHellman',
+//   'getHashes',
+//   'hkdf',
+//   'hkdfSync',
+//   'pbkdf2',
+//   'pbkdf2Sync',
+//   'generateKeyPair',
+//   'generateKeyPairSync',
+//   'generateKey',
+//   'generateKeySync',
+//   'privateDecrypt',
+//   'privateEncrypt',
+//   'publicDecrypt',
+//   'publicEncrypt',
+//   'randomBytes',
+//   'randomFill',
+//   'randomFillSync',
+//   'randomInt',
+//   'randomUUID',
+//   'scrypt',
+//   'scryptSync',
+//   'sign',
+//   'setEngine',
+//   'timingSafeEqual',
+//   'getFips',
+//   'setFips',
+//   'verify',
+//   'Certificate',
+//   'Cipher',
+//   'Cipheriv',
+//   'Decipher',
+//   'Decipheriv',
+//   'DiffieHellman',
+//   'DiffieHellmanGroup',
+//   'ECDH',
+//   'Hash',
+//   'Hmac',
+//   'KeyObject',
+//   'Sign',
+//   'Verify',
+//   'X509Certificate',
+//   'secureHeapUsed',
+//   'constants',
+//   'webcrypto',
+//   'subtle',
+//   'getRandomValues'
+// ]
+// undefined
+// > const id = crypto.randomUUID()
+// undefined
+// > console.log(id)
+// 4baad70d-476c-486e-ba64-91ccaeafbc96
+// undefined
+// > .exit
+// PS C:\Users\gabri\OneDrive\Desktop\ESERCIZI NODE.JS\Esegui il REPL di Node js> 
