b209c583b1f48526a5a36b66923422f53139c2fe
Maurilio Valenti <166375402+Maurilio93@users.noreply.github.com>
2024-10-25 12:04:03 +0200

    Add files via upload

---
+++ JWT Auth parte 1/passport.ts
@@ -0,0 +1,24 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+import passport from "passport";
+import passportJWT from "passport-jwt";
+import { db } from "./db";
+
+const SECRET = process.env.SECRET || '';
+
+passport.use(
+  new passportJWT.Strategy(
+    {
+      secretOrKey: SECRET,
+      jwtFromRequest: passportJWT.ExtractJwt.fromAuthHeaderAsBearerToken(),
+    },
+    async (payload, done) => {
+      try {
+        const user = await db.one(`SELECT * FROM users WHERE id=$1`, payload.id);
+        return user ? done(null, user) : done(new Error("user not found"));
+      } catch (error) {
+        done(error);
+      }
+    }
+  )
+);
